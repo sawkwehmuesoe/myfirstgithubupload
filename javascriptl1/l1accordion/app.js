5d5d4f42ad4b33572bdbd112b2f724d526f85cea
saw kweh mue soe <sawkwehmuesoe@gmail.com>
2022-08-08 22:49:47 +0630

    i am here to love you

---
+++ javascriptl1/l1accordion/app.js
@@ -0,0 +1,35 @@
+var getacctitles = document.getElementsByClassName("acctitle");
+// console.log(getacctitles);
+var getacccontent = document.getElementsByClassName("acccontent");
+// console.log(getacccontent);
+
+
+	for(var x = 0; x < getacctitles.length; x++){
+
+		// console.log(x);
+
+		getacctitles[x].addEventListener('click',function(e){
+
+			// console.log(e.target);
+			// console.log(this);
+
+			this.classList.toggle("active");
+
+			var getcontent = this.nextElementSibling;
+			// console.log(getcontent);
+
+			if(getcontent.style.height){
+				getcontent.style.height = null;
+			}else{
+				// console.log(getcontent.scrollHeight);
+				getcontent.style.height = getcontent.scrollHeight + "px"; 
+			}
+
+		});
+
+		if(getacctitles[x].classList.contains("active")){
+			getacccontent[x].style.height = getacccontent[x].scrollHeight + "px";
+		}
+
+
+	}
